aece7af4113d31ee0c6009ff97c75fee8a1c466d
Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-01-19 22:17:05 +0530

    updated conts

---
+++ lib/consts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+//App color.
+
+const Color g2 = Colors.black;
+const Color kInputColor = Colors.white;
+const Color kGrey = Color.fromRGBO(64, 64, 64, 1);
+
+const Color kWhiteColor = Colors.white;
+const Color kRedColor = Color(0xFFB71C1C);
+const Color g1 = Color.fromRGBO(28, 29, 33, 1);
+const Color kButtonColor = Color.fromRGBO(238, 209, 0, 1);
+ 
